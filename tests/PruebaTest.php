e2a0728acbc0874c0970f9987dfbfa8b9182e651
Monica-Perez <98490517+Monica-Perez@users.noreply.github.com>
2025-10-17 01:58:22 +0000

    Update deploy to output generated at 373c918

--- tests/PruebaTest.php
+++
@@ -1,9 +0,0 @@
-<?php
-use PHPUnit\Framework\TestCase;
-require __DIR__ . '/../src/Prueba.php';
-
-final class PruebaTest extends TestCase {
-  public function testPrueba() {
-    $this->assertSame("Hola, Monica", saludo("Monica"));
-  }
-}
